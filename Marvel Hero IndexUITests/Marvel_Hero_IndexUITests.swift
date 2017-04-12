bc4c683b9bec769f46eefc151e9877a458a77d79
Mike Stuck <mikestuck@msn.com>
2017-04-11 22:46:42 -0400

    Initial Commit

---
+++ Marvel Hero IndexUITests/Marvel_Hero_IndexUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Marvel_Hero_IndexUITests.swift
+//  Marvel Hero IndexUITests
+//
+//  Created by Mike Stuck on 4/11/17.
+//  Copyright © 2017 Mike Stucj. All rights reserved.
+//
+
+import XCTest
+
+class Marvel_Hero_IndexUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
